f6cbd24acc6cec1863ab0fdb761b7cc06efee52f
Pavel Davydov <typical000@gmail.com>
2022-01-10 19:10:35 +0200

    Change root route.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import {Injectable} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getApp(): string {
+    return `
+      <center>
+        <h1>Service is not supported</h1>
+        <p>
+          We apologize for the inconvenience, the service is not supported for you.
+        </p>
+      </center>
+    `;
   }
 }
